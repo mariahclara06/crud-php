2f6c0935b88b2711cdc31d97fd6374d79e18ea8a
✨ Maria Clara ✨ <124745626+mariahclara06@users.noreply.github.com>
2023-05-04 19:14:10 -0300

    Add files via upload

---
+++ Categoria.Class.php
@@ -0,0 +1,45 @@
+<?php
+
+include_once("interfaces/crud.php");
+include_once("classes/DB.Class.php");
+
+    class Categoria implements crud{
+        private $id;
+        private $nome;
+        
+        public function __construct($id=false){
+             if($id){
+               $sql = "SELECT * FROM Produtos where id = ?";
+               $conexao = DB::conexao();
+           $stmt = $conexao->prepare($sql);
+           $stmt->bindParam(1,$id, PDO::PARAM_INT);
+           $stmt->execute();
+           foreach($stmt as $obj){
+             $this->setId($obj['id']);
+             $this->setNome($obj['nome']);
+            }
+          }
+        }
+
+        public function setid($id){
+            $this->id=$id;
+       }
+       public function getid(){
+            return $this->id;
+       }
+       public function setNome($nome){
+            $this->nome=$nome;
+       }
+       public function getNome(){
+            return $this->nome;
+       }
+
+       
+      public function adicionar(){}
+      public function listar(){}
+      public function atualizar(){}
+      public function excluir(){}
+
+    }
+
+?>
